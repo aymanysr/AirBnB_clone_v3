3595b5632417160c0f58062cd480928cabf36d63
aimane yousr <aimaneyousr@gmail.com>
2024-05-01 11:22:35 +0100

    Creating an endpoint that retrieves the number of each objects by type, using count method from storage

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,7 @@ create flask app; app_views
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status')
@@ -12,3 +13,17 @@ def api_status():
     """return status"""
     resp = {"status": "OK"}
     return jsonify(resp)
+
+
+@app_views.route('/stats')
+def api_stats():
+    """return stats"""
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats)
