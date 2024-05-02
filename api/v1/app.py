946c4c8eab62659f3c76d50fb95a2ad3d9d9cdeb
aimane yousr <aimaneyousr@gmail.com>
2024-05-02 18:52:00 +0100

    Refactor app.py to add CORS support and update URL mapping

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,13 +6,17 @@ to the Flask app.
 
 from os import getenv
 from flask import Flask, jsonify
+from flask_cors import CORS
 from api.v1.views import app_views
 from models import storage
 
 
 app = Flask(__name__)
 
+CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
+
 app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
